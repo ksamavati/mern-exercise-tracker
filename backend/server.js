3d0035d502d76457b29665a90c48a26d57126361
ksamavati <kayvonsamavati@gmail.com>
2022-08-23 12:31:34 -0400

    push 7

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+
+mongoose.connect(String(uri), { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
