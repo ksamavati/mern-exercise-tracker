68fd58e35bdfd916818308b309a4c52724089c30
ksamavati <kayvonsamavati@gmail.com>
2022-08-24 14:18:20 -0400

    1

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,8 @@ const usersRouter = require('./routes/users');
 // if (process.env.NODE_ENV === "production") {
 app.use(express.static('../build'));
 app.get('/', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+	res.send(__dirname);
+	// res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 })
 // }
 
