eafad6513fcf1e3f1c56b42b3f9730e3a3c00239
ksamavati <kayvonsamavati@gmail.com>
2022-08-24 12:25:02 -0400

    push 9

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ const usersRouter = require('./routes/users');
 
 // if this is running on live server (Heroku)
 // if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static('../build'));
-// 	app.get('*', (req, res_) => {
-// 		req.sendFile(path.resolve(__dirname, '../build', 'index.html'));
-// 	})
+app.use(express.static('../build'));
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+})
 // }
 
 // app.get('/', function (req, res) {
