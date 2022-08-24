bd1b5db336af2d6aef2fb1de789d385bd5f13a55
ksamavati <kayvonsamavati@gmail.com>
2022-08-24 12:45:34 -0400

    1

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const usersRouter = require('./routes/users');
 
 // if this is running on live server (Heroku)
 // if (process.env.NODE_ENV === "production") {
-app.use(express.static('../'));
+app.use(express.static('../build'));
 app.get('/', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 })
