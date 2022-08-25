df5c18511163dc9aac054d569044be922e86e32b
ksamavati <kayvonsamavati@gmail.com>
2022-08-25 14:13:13 -0400

    1

--- src/setURL.js
+++ src/setURL.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+import dotenv from 'dotenv'
+dotenv.config()
 const SERVER_URL = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://ks-exercise-tracker.herokuapp.com"
 
 export default SERVER_URL
